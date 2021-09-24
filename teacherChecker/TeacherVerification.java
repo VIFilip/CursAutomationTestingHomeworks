e2db8ab076e3635773d436d04958c1b95996a5df
VIFilip <vlad_1611@yahoo.com>
2021-09-24 10:23:43 +0300

    Homework3_updated

--- teacherChecker/TeacherVerification.java
+++ teacherChecker/TeacherVerification.java
@@ -3,9 +3,15 @@ package teacherChecker;
 public class TeacherVerification {
 
 	public static void main(String[] args) {
-		Teacher teacher1 = new Teacher();
-		System.out.println(teacher1.verify());
+		
+		
+		
+		Qualification qualif =  new Qualification("Java", 4, "Afternoon"); 
+		qualif.verify();
 
+		Qualification qualif2 =  new Qualification("Python", 5, "Afternnon");
+		qualif2.verify();
+		
 	}
 
 }
