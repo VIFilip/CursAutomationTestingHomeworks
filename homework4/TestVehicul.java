d87b61c8235b7d262da407f0d4a7039f96673734
VIFilip <vlad_1611@yahoo.com>
2021-09-28 13:58:39 +0300

    homewor4_updated

--- homework4/TestVehicul.java
+++ homework4/TestVehicul.java
@@ -5,35 +5,30 @@ public class TestVehicul {
 	public static void main(String[] args) {
 		
 		//instantiere obiecte de tip vehicul cu parametri si apelare metode
-		Vehicul bicicleta = new Bicicleta("Mountainbike", 0, 50);
-		//bicicleta.verificaMotorizarea();//nu reusesc sa apelez de aici
+		Vehicul bicicleta = new Bicicleta("Mountainbike", 0, 50);		
 		bicicleta.detaliiVehicul();
+		verificaMotorizarea(bicicleta);
+		
 		
 		Vehicul masina = new Masina("Dacia", 5, 200);
-		//masina.verificaMotorizarea();//nu reusesc sa apelez de aici
 		masina.detaliiVehicul();
-		
+		verificaMotorizarea(masina);
 		
 		
 
 	}
 	
-	//obiect de tip vehicul primit ca parametru de metoda verificaMotorizarea
-			Bicicleta bicicleta = new Bicicleta("Electrica", 100, 2);
+ 		
+	public static void verificaMotorizarea(Vehicul objVehicul) {
 			
-			
+			System.out.println("Nume vehicul este: " + objVehicul.nume() + " si are motorizarea: " + objVehicul.motorizare());
+		}
 
-	public void verificaMotorizarea() {
-		
-		System.out.println(bicicleta.nume() + bicicleta.motorizare());
-		
-
-		
-		
+	
 		
 		
 	}
 	
 	
 
-}
+
